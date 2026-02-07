2be7838b6526564c19d565dffe252903d275288a
Diego Rapichan <direrapichan@gmail.com>
2026-02-07 20:39:24 -0300

    Initial commit - empty backend

--- autoflex-backend/src/main/java/com/autoflex/inventory/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.autoflex.inventory.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
